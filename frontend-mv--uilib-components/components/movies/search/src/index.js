187ce5789078e577adbbb07045cd22d714a358e1
Edgar Frías <edfriasji@gmail.com>
2019-06-19 10:05:08 +0200

    Adevinta's pet project
    Domain and react components initial commit

---
+++ frontend-mv--uilib-components/components/movies/search/src/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+// import PropTypes from 'prop-types'
+
+class MovieSearch extends Component {
+  render() {
+    return (
+      <div className="sui-MovieSearch">
+        <h1>MovieSearch</h1>
+      </div>
+    )
+  }
+}
+
+MovieSearch.displayName = 'MovieSearch'
+
+// Remove these comments if you need
+// MovieSearch.contextTypes = {i18n: PropTypes.object}
+// MovieSearch.propTypes = {}
+// MovieSearch.defaultProps = {}
+
+export default MovieSearch
